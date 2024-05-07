b46b92d5c52435a3ee6b1b9d018123ddcca60ac6
Loïc Dörr <loic.debeuf@gmail.com>
2024-05-07 13:07:33 +0200

    test

--- api/projects.ts
+++ api/projects.ts
@@ -1,3 +1,9 @@
 export function GET(request: Request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+  const data = [
+    { id: 1, name: 'Project 1', description: 'Description 1' },
+    { id: 2, name: 'Project 2', description: 'Description 2' },
+    { id: 3, name: 'Project 3', description: 'Description 3' },
+  ]
+
+  return new Response(JSON.stringify(data))
 }
