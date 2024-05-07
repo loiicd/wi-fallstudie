a64f49dc695763d301cc3d0922ddd948efaedc72
Loïc Dörr <loic.debeuf@gmail.com>
2024-05-07 12:32:04 +0200

    test

--- src/api/projects.ts
+++ src/api/projects.ts
@@ -1,3 +1,7 @@
-export const GET = (reqeuest: Request) => {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`)
+// export const GET = (reqeuest: Request) => {
+//   return new Response(`Hello from ${process.env.VERCEL_REGION}`)
+// }
+
+export function GET(request: Request) {
+  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
