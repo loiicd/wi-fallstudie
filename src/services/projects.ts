9cd38b1691cbedab60a13113c7b2402dd62239ca
jacobrhn <111424624+jacobrhn@users.noreply.github.com>
2024-05-21 21:43:41 +0200

    SH-25 delete project

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -14,23 +14,10 @@ export const getProjectsById = async (id: string): Promise<Project[]> => {
 export const postProject = async (projectFormData: ProjectFormData): Promise<void> => {
   await axios.post('/api/project', projectFormData)
 }
+
 export const deleteProject = async (id: string): Promise<void> => {
-  try {
-    await axios.delete('/api/project', { params: { id: id } })
-  } catch (error) {
-    if ((error as AxiosError).response) {
-      // The request was made and the server responded with a status code
-      console.log((error as AxiosError).response?.data)
-      console.log((error as AxiosError).response?.status)
-      console.log((error as AxiosError).response?.headers)
-    } else if ((error as AxiosError).request) {
-      // The request was made but no response was received
-      console.log((error as AxiosError).request)
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log('Error', (error as AxiosError).message)
-    }
-    console.log((error as AxiosError).config)
-    throw error // Rethrow the error to handle it in the calling code
-  }
+  await axios.delete('/api/project', { params: { id: id }})
+  .catch((error: AxiosError) => {
+    throw new Error(error.response?.data as string)
+  })
 }
