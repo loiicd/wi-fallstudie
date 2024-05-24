7f0e232e8d87207cb1f72003a1eaad1721e50b6b
jacobrhn <111424624+jacobrhn@users.noreply.github.com>
2024-05-24 21:34:19 +0200

    revert

--- src/components/projectsTable.tsx
+++ src/components/projectsTable.tsx
@@ -13,7 +13,6 @@ import RoleProvider from './RoleProvider'
 import StatusChip from './statusChip'
 import { ApiResponse } from '../types/apiResponse'
 import { useNavigate } from 'react-router-dom'
-import { Typography } from '@mui/material'
 
 const columns: GridColDef<(any)[number]>[] = [
   {
@@ -30,7 +29,7 @@ const columns: GridColDef<(any)[number]>[] = [
     headerName: 'Titel',
     width: 150,
     editable: false,
-    type: 'string',   
+    type: 'string',
   },
   {
     field: 'start_date',
@@ -65,19 +64,25 @@ const columns: GridColDef<(any)[number]>[] = [
 export default function ProjectsTable() {
   const navigate = useNavigate()
   const [searchTerm, setSearchTerm] = useState<string>('')
-  const [openProjectDetailHover, setOpenProjectDetailHover] = useState<boolean>(false)
+  const [openProjectDetailDialog, setOpenProjectDetailDialog] = useState<boolean>(false)
   const [openAddProjectDialog, setOpenAddProjectDialog] = useState<boolean>(false)
   const [openEditProjectDialog, setOpenEditProjectDialog] = useState<boolean>(false)
   const [project, setProject] = useState<null | any>(null)
   const [projects, setProjects] = useState<ApiResponse<Project[]>>({ state: 'loading' })
+  const [searchedProjects, setSearchedProjects] = useState<Project[]>([])
 
   useEffect(() => {
     getProjects()
       .then(projects => setProjects({ state: 'success', data: projects}))
       .catch(error => setProjects({ state: 'error', message: error}))
-      console.log('projects state: ' + projects.state)
-  }, [searchTerm, openAddProjectDialog, openProjectDetailHover, openEditProjectDialog])
+  }, [openAddProjectDialog, openProjectDetailDialog, openEditProjectDialog])
 
+  useEffect(() => {
+    if (projects.state === 'success') {
+      const filteredProjects = projects.data.filter(project => project.title.includes(searchTerm))
+      setSearchedProjects(filteredProjects)
+    }
+  }, [searchTerm, projects])
 
   const handleCellClick = (project: any) => {
     setProject(project)
@@ -85,7 +90,7 @@ export default function ProjectsTable() {
   }
 
   const editProject = () => {
-    setOpenProjectDetailHover(false);
+    setOpenProjectDetailDialog(false);
     setOpenEditProjectDialog(true);
   };
 
@@ -93,35 +98,9 @@ export default function ProjectsTable() {
     setSearchTerm(event.target.value)
   }
 
-  const handleRowHovered = (event: React.MouseEvent<HTMLElement>) => {
-    const rowId = event.currentTarget?.dataset?.id
-    document.dispatchEvent(
-      new CustomEvent(`row${rowId}HoverChange`, { detail: { hovered: true } })
-    )
-    if (projects.state === 'success') {
-      const foundProject = projects.data.find((project: { id: string | undefined }) => project.id === rowId);
-      setProject(foundProject);
-      setOpenProjectDetailHover(true);
-      console.log('found: ' + foundProject?.title);
-      console.log('set: ' + project?.title);
-    }
-}
-
-  const handleRowLeaved = (event: React.MouseEvent<HTMLElement>) => {
-    const rowId = event.currentTarget?.dataset?.id;
-    document.dispatchEvent(
-      new CustomEvent(`row${rowId}HoverChange`, { detail: { hovered: false } })
-    )
-    setOpenProjectDetailHover(false)
-    setProject(null)
-  }
-
   return (
     <>
       <Card>
-      {/* openProjectDetailHover && project ? <ProjectDetailDialog project={project} open={openProjectDetailHover} handleClose={() => setOpenProjectDetailHover(false)} handleEdit={() => editProject()} /> : null */
-      openProjectDetailHover && project ? <Typography>{project.title}</Typography> : <Typography>Titel</Typography>
-      }
         <Stack direction='row' spacing={2} justifyContent="flex-end" alignItems="center" sx={{ margin: 2 }}>
           <TextField placeholder='Suche ...' size='small' onChange={handleSearch} />
           <RoleProvider roles={['projekteigner', 'projektmanager', 'administrator']} type='include'>
@@ -130,16 +109,10 @@ export default function ProjectsTable() {
         </Stack>
         <DataGrid
           loading={projects.state === 'loading'}
-          sx={{ minHeight: 50}}
-          rows={projects.state === 'success' ? projects.data : []}
+          autoHeight
+          rows={projects.state === 'success' ? searchedProjects : []}
           columns={columns}
           onCellClick={(params) => handleCellClick(params.row)}
-          slotProps={
-            { row: { 
-              onMouseEnter: handleRowHovered, 
-              onMouseLeave: handleRowLeaved
-            } }
-          }
           pageSizeOptions={[5, 10, 15]}
           initialState={{
             pagination: {
@@ -163,9 +136,7 @@ export default function ProjectsTable() {
           disableColumnResize
         />
       </Card>
-      {/**  
-      {openProjectDetailDialog && project ? <ProjectDetailDialog project={project} open={openProjectDetailDialog} handleClose={() => setOpenProjectDetailHover(false)} handleEdit={() => editProject()} /> : null}
-      */}
+      {openProjectDetailDialog && project ? <ProjectDetailDialog project={project} open={openProjectDetailDialog} handleClose={() => setOpenProjectDetailDialog(false)} handleEdit={() => editProject()} /> : null}
       {openAddProjectDialog ? <AddProjectDialog open={openAddProjectDialog} handleClose={() => setOpenAddProjectDialog(false)} /> : null}
       {openEditProjectDialog && project ? <AddProjectDialog open={openEditProjectDialog} handleClose={() => setOpenEditProjectDialog(false)} project={project} /> : null}
     </>
