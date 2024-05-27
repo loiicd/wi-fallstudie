dbd627d61cab34088c0edfed715cac10a18c460a
Doerr, Loic <loic.doerr@haufe-akademie.de>
2024-05-27 09:10:07 +0200

    refactoring

--- src/components/compareCard.tsx
+++ src/components/compareCard.tsx
@@ -9,6 +9,24 @@ import OpenInNewIcon from '@mui/icons-material/OpenInNew'
 import Rating from '@mui/material/Rating'
 import { FunctionComponent } from 'react'
 
+interface CompareSectionProps {
+  items: { label: string, value?: any }[]
+}
+
+const CompareSection: FunctionComponent<CompareSectionProps> = ({ items }) => {
+  return (
+    <>
+      <Divider sx={{ margin: 2 }} />
+      {items.map((item) => (
+        <>
+          <Typography variant='overline'>{item.label}</Typography>
+          <Typography variant='h6'>{item.value ? item.value : '-'}</Typography>  
+        </>
+      ))}
+    </>
+  )
+}
+
 interface CompareCardProps {
   project: Project
 }
@@ -21,17 +39,23 @@ const CompareCard: FunctionComponent<CompareCardProps> = ({ project }) => {
       <IconButton sx={{ position: 'absolute', right: 0, margin: 1 }} onClick={() => navigate(`/project/${project.id}`)}><OpenInNewIcon /></IconButton>
       <CardContent sx={{ textAlign: 'center' }}>
         <Typography variant='h6'>{project.title}</Typography>
-        <Divider sx={{ margin: 4 }} />
-        <Typography variant='overline'>Startdatum</Typography>
-        <Typography variant='h6'>{project.start_date ? new Date(project.start_date).toLocaleDateString() : '-'}</Typography>
-        <Typography variant='overline'>Enddatum</Typography>
-        <Typography variant='h6'>{project.end_date ? new Date(project.end_date).toLocaleDateString() : '-'}</Typography>
-        <Divider sx={{ margin: 4 }} />
-        <Typography variant='overline'>Projektleiter</Typography>
-        <Typography variant='h6'>{project.project_lead ? `${project.project_lead.firstname} ${project.project_lead.lastname}` : '-' }</Typography>
-        <Typography variant='overline'>Stellv. Projektleiter</Typography>
-        <Typography variant='h6'>{project.sub_project_lead ? `${project.sub_project_lead.firstname} ${project.sub_project_lead.lastname}` : '-' }</Typography>
-        <Divider sx={{ margin: 4 }} />
+
+        <CompareSection items={[
+          { label: 'Abteilung', value: project.department },
+          { label: 'Standort', value: project.location }
+        ]} />
+
+        <CompareSection items={[
+          { label: 'Startdatum', value: project.start_date ? new Date(project.start_date).toLocaleDateString() : undefined },
+          { label: 'Enddatum', value: project.end_date ? new Date(project.end_date).toLocaleDateString() : undefined }
+        ]} />
+
+        <CompareSection items={[
+          { label: 'Projektleiter', value: project.project_lead ? `${project.project_lead.firstname} ${project.project_lead.lastname}` : undefined },
+          { label: 'Stellv. Projektleiter', value: project.sub_project_lead ? `${project.sub_project_lead.firstname} ${project.sub_project_lead.lastname}` : undefined }
+        ]} />
+
+        <Divider sx={{ margin: 2 }} />
         <Rating value={project.rates.reduce((sum, rate) => sum + rate.rate, 0) / project.rates.length} readOnly precision={0.1} />
       </CardContent>
     </Card>
