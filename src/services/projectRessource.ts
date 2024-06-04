c9b0fac5236169a78ccfa5d863195e9511f6657f
jacobrhn <111424624+jacobrhn@users.noreply.github.com>
2024-06-04 13:35:21 +0200

    right file

--- src/services/projectRessource.ts
+++ src/services/projectRessource.ts
@@ -3,14 +3,14 @@ import ProjectRessourceGeneric from "../types/projectResourceGeneric"
 
 export const saveRessource = async (ressource: ProjectRessourceGeneric): Promise<void> => {
   console.log("ressource", ressource)
-  await axios.post('/api/projectRelations', ressource)
+  await axios.post('/api/projectRessources', ressource)
 }
 
 export const deleteRessource = async (ressource_id: string): Promise<void> => {
-  await axios.delete(`/api/projectRelations`, { data: { id: ressource_id } })
+  await axios.delete(`/api/projectRessources`, { data: { id: ressource_id } })
 }
 
 export const getProjectRessourcesByType = async (project_id: string, type: string): Promise<ProjectRessourceGeneric[]> => {
-  const response = await axios.get(`/api/projectRelations`, { params: { project_id, type } })
+  const response = await axios.get(`/api/projectRessources`, { params: { project_id, type } })
   return response.data
 }
